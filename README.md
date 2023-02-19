3047d3a4d1d70bdc3d7d2e2464dbf76956c0f547
Luis Henrique Pinto Arandas <l.arandas0220221@arts.ac.uk>
2023-02-19 17:37:51 +0000

    Update .gitignore, README.md, and 4 more files...

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# dpg-nuitka-example
-simple cross platform dpg app with nuitka
+
+# dpg-nuitka-example <br>
+
+simple cross platform dpg app with nuitka <br>
+compiling on MacOS 12.2<br>
+
+## macos installation <br>
+cd xyz/dpg-nuitka-example<br>
+python3 -m venv venv && source venv/bin/activate<br>
+pip3 install dearpygui<br>
+pip3 install Nuitka<br>
+pip3 install pyinstaller<br>
+// wget http://nuitka.net/releases/Nuitka-0.7.6.tar.gz<br>
+// tar -xf Nuitka-0.7.6.tar.gz<br>
+// cd Nuitka-0.7.6 && python3 setup.py install<br>
+python3 -m nuitka --macos-create-app-bundle --macos-disable-console --macos-app-icon=./images/icons/appstore.icns --output-dir=./dist/ main.py
+pyinstaller --onefile --icon=./images/icons/appstore.icns --name=dpgnuitkaexample main.py<br>
+
+## windows installation <br>
+
+todo <br>
+
+## linux installation <br>
+
+todo <br>
---
+++ main.py
@@ -0,0 +1,41 @@
+
+
+# luis arandas 16-02-2022
+# very simple two button dpg project compiling for nuitka on all platforms
+# supposed to be used as a fork to other app projects
+
+import os
+
+dist_folder = "./dist"
+if not os.path.exists(dist_folder):
+    os.mkdir(dist_folder)
+
+# setup application
+
+import dearpygui.dearpygui as dpg
+
+def save_callback():
+    print("Save Clicked")
+
+dpg.create_context()
+dpg.create_viewport(title=' ', width=350, height=200, resizable=False)
+
+def dpg_exit_callback():
+    dpg.stop_dearpygui()
+dpg.set_viewport_pos([0,0])
+dpg.setup_dearpygui()
+
+with dpg.window(tag="mainwindow", pos=(0,0)):
+    dpg.add_text("simple dpg nuitka example")
+    dpg.add_button(label="Save", callback=save_callback)
+    dpg.add_input_text(label="string")
+    dpg.add_slider_float(label="float")
+
+dpg.show_viewport()
+dpg.set_exit_callback(dpg_exit_callback)
+dpg.set_global_font_scale(1)
+# dpg.maximize_viewport()
+dpg.set_primary_window("mainwindow", True)
+dpg.set_viewport_resizable(False)
+dpg.start_dearpygui()
+dpg.destroy_context()
